9738f7af8c4eac55ea82db01a5f30fd4f0819b45
Renan Brayner <renanbrayner@gmail.com>
2025-04-16 15:17:18 -0300

    refactor(frontend): use slot in BaseInput instead of passing props

--- frontend/src/components/BaseInput.vue
+++ frontend/src/components/BaseInput.vue
@@ -1,37 +1,22 @@
 <script setup lang="ts">
 import { defineProps } from 'vue'
 import FloatLabel from 'primevue/floatlabel'
-import InputText from 'primevue/inputtext'
-import InputMask from 'primevue/inputmask'
 import InputIcon from 'primevue/inputicon'
 import IconField from 'primevue/iconfield'
 
 defineProps<{
-  id: string
   label: string
   icon?: string
-  modelValue: string
   mask?: string
 }>()
-
-const emit = defineEmits(['update:modelValue'])
 </script>
 
 <template>
   <FloatLabel variant="in">
     <IconField>
-      <InputIcon v-if="icon" :class="`pi ${icon}`" />
-      <component
-        :is="mask ? InputMask : InputText"
-        :mask="mask"
-        :autoClear="false"
-        :id="id"
-        :modelValue="modelValue"
-        @update:modelValue="emit('update:modelValue', $event)"
-        autocomplete="off"
-        variant="filled"
-      />
+      <InputIcon v-if="icon" :class="icon" />
+      <slot />
     </IconField>
-    <label :for="id">{{ label }}</label>
+    <label>{{ label }}</label>
   </FloatLabel>
 </template>
--- frontend/src/components/contacts/form/ContactForm.vue
+++ frontend/src/components/contacts/form/ContactForm.vue
@@ -5,6 +5,8 @@ import Button from 'primevue/button'
 import { RouterLink } from 'vue-router'
 import { useContacts } from '@/composables/useContacts'
 import { useToast } from 'primevue/usetoast'
+import InputMask from 'primevue/inputmask'
+import InputText from 'primevue/inputtext'
 
 const { addContact } = useContacts()
 const toast = useToast()
@@ -35,15 +37,15 @@ const handleSubmit = async () => {
 
 <template>
   <form @submit.prevent="handleSubmit" class="flex flex-col gap-4">
-    <BaseInput id="name" label="Nome" icon="pi pi-user" v-model="name" />
-    <BaseInput
-      mask="(99) 99999-9999"
-      id="phone"
-      label="Telefone"
-      icon="pi pi-phone"
-      v-model="phone"
-    />
-    <BaseInput id="email" label="E-mail" icon="pi pi-envelope" v-model="email" />
+    <BaseInput label="Nome" icon="pi pi-user">
+      <InputText v-model="name" />
+    </BaseInput>
+    <BaseInput label="Telefone" icon="pi pi-phone">
+      <InputMask :autoClear="false" mask="(99) 99999-9999" v-model="phone" />
+    </BaseInput>
+    <BaseInput label="E-mail" icon="pi pi-envelope">
+      <InputText v-model="email" />
+    </BaseInput>
     <div class="flex gap-4">
       <RouterLink class="flex-1" to="/">
         <Button fluid outlined label="Cancelar" icon="pi pi-times" />
